9235ab944c42b5eeec5c16e4d2543b44fe7969f3
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-02-20 09:19:13 -0500

    Add project files.

---
+++ p3rpc.femc.yukaributfemctrueadachi/Mod.cs
@@ -0,0 +1,110 @@
+﻿using p3rpc.femc.yukaributfemctrueadachi.Configuration;
+using p3rpc.femc.yukaributfemctrueadachi.Template;
+using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using UnrealEssentials.Interfaces;
+
+namespace p3rpc.femc.yukaributfemctrueadachi
+{
+	/// <summary>
+	/// Your mod logic goes here.
+	/// </summary>
+	public class Mod : ModBase // <= Do not Remove.
+	{
+		/// <summary>
+		/// Provides access to the mod loader API.
+		/// </summary>
+		private readonly IModLoader _modLoader;
+
+		/// <summary>
+		/// Provides access to the Reloaded.Hooks API.
+		/// </summary>
+		/// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+		private readonly IReloadedHooks? _hooks;
+
+		/// <summary>
+		/// Provides access to the Reloaded logger.
+		/// </summary>
+		private readonly ILogger _logger;
+
+		/// <summary>
+		/// Entry point into the mod, instance that created this class.
+		/// </summary>
+		private readonly IMod _owner;
+
+		/// <summary>
+		/// Provides access to this mod's configuration.
+		/// </summary>
+		private Config _configuration;
+
+		/// <summary>
+		/// The configuration of the currently executing mod.
+		/// </summary>
+		private readonly IModConfig _modConfig;
+
+		public Mod(ModContext context)
+		{
+			_modLoader = context.ModLoader;
+			_hooks = context.Hooks;
+			_logger = context.Logger;
+			_owner = context.Owner;
+			_configuration = context.Configuration;
+			_modConfig = context.ModConfig;
+
+			var enabledMods = this._modLoader.GetAppConfig().EnabledMods;
+			var femcEnabled = enabledMods.Contains("p3rpc.femc");
+
+			List<string> foldersToAdd = new List<string>();
+			if (!femcEnabled)
+			{
+				foldersToAdd.Add("TestFolder");
+			}
+
+			var unrealEssentialsController = _modLoader.GetController<IUnrealEssentials>();
+			if (unrealEssentialsController == null || !unrealEssentialsController.TryGetTarget(out var unrealEssentials))
+			{
+				_logger.WriteLine($"Femc over Yukari Unable to get controller for Unreal Essentials, please be sure to ping @DniweTamp in the femc reloaded server.", System.Drawing.Color.Pink);
+				return;
+			}
+
+			var modDir = _modLoader.GetDirectoryForModId(_modConfig.ModId);
+
+			foreach (var folderName in foldersToAdd)
+			{
+				var filesPath = Path.Combine(modDir, folderName);
+				_logger.WriteLine($"Loading folder: {folderName}", System.Drawing.Color.Pink);
+				unrealEssentials.AddFromFolder(filesPath);
+			}
+
+
+			// For more information about this template, please see
+			// https://reloaded-project.github.io/Reloaded-II/ModTemplate/
+
+			// If you want to implement e.g. unload support in your mod,
+			// and some other neat features, override the methods in ModBase.
+
+			// TODO: Implement some mod logic
+		}
+
+		public void if_you_find_this_dm_me_for_20_bucks_true()
+		{
+			// :true:
+		}
+
+		#region Standard Overrides
+		public override void ConfigurationUpdated(Config configuration)
+		{
+			// Apply settings from configuration.
+			// ... your code here.
+			_configuration = configuration;
+			_logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+		}
+		#endregion
+
+		#region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		public Mod() { }
+#pragma warning restore CS8618
+		#endregion
+	}
+}
